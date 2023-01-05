bc60fea5f168f7c429e6d0ed6913d2dc9492a62a
Alex <akorolonek@yahoo.com>
2023-01-05 15:56:00 +0100

    Added function playRound

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 
 
+let computerSelection = getComputerSelection();
+let playerSelection = "Rock"
 
-
-const getComputerChoice = () => {
-    const choices = ['rock', 'Paper', 'Scissor'];
-    const randomIndex = Math.floor(Math.random() * 3);
-    return choices[randomIndex];
+//Generates a ComputerChoice
+function getComputerSelection() {
+    const selection = ['Rock', 'Paper', 'Scissor', 'Spock', 'Lizard'];
+    const randomIndex = Math.floor(Math.random() * selection.length);
+    return selection[randomIndex];
+}
+//Changes User Input and Computer Choice to lower case to prevent collisions with Notation
+function toLowerCase() {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 }
 
-console.log(getComputerChoice()); // Gibt zufällig "Stein", "Papier" oder "Schere" aus
+
+function playRound(playerSelection, computerSelection) {
+    toLowerCase();
+      // Definiere ein Objekt mit den Regeln für das Spiel
+  let rules = {
+    rock: { losesTo: ["paper", "spock"] },
+    paper: { losesTo: ["scissors", "lizard"] },
+    scissors: { losesTo: ["rock", "spock"] },
+    spock: { losesTo: ["paper", "lizard"] },
+    lizard: { losesTo: ["rock", "scissors"] }
+  };
+
+  if (playerSelection === computerSelection){
+    return "Draw!"
+  }
+  if (rules[playerSelection].losesTo.includes(computerSelection)) {
+    return `You Lost! ${computerSelection} beats ${playerSelection}`;
+} else {
+  return `You Win ${playerSelection} beats ${computerSelection}`;
+
+}
+}
+
+console.log(playRound("rock", "scissors")); // You Win! Rock beats Scissors
+console.log(playRound("lizard", "rock")); // You Lose! Rock beats Lizard
+console.log(playRound("spock", "paper")); // You Lose! Paper beats Spock
+console.log(playRound("scissors", "scissors")); // Draw!
