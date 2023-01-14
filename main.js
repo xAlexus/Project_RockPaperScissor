b67c55c1204e521b65932a996c69debc63b995fe
Alex <akorolonek@yahoo.com>
2023-01-14 11:57:23 +0100

    Added new CSS styles and HTML layout for Rock Paper Scissors Lizard Spock game including font import, background color, padding, and text alignment.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
+let playerScore = 0;
+let computerScore = 0;
 
-let computerSelection = getComputerSelection();
-let playerSelection = "Rock"
 let rules = {
     rock: { losesTo: ["paper", "spock"] },
     paper: { losesTo: ["scissors", "lizard"] },
@@ -24,18 +24,48 @@ function toLowerCase() {
 
 
 function playRound(playerSelection, computerSelection) {
-    toLowerCase();
+  // Make copies of the arguments in lowercase
+  let player = playerSelection.toLowerCase();
+  let computer = computerSelection.toLowerCase();
 
-  if (playerSelection === computerSelection){
+  if (player === computer){
     return "Draw!"
   }
-  if (rules[playerSelection].losesTo.includes(computerSelection)) {
-    return `You Lost! ${computerSelection} beats ${playerSelection}`;
-} else {
-  return `You Win ${playerSelection} beats ${computerSelection}`;
+  if (rules[player].losesTo.includes(computer)) {
+    return `You Lost! ${computer} beats ${player}`;
+  } else {
+    return `You Win ${player} beats ${computer}`;
+  }
+}
 
+function game() {
+  let playerSelection = prompt("Rock, Paper, Scissors, Spock, or Lizard?");
+
+  for (let i = 0; i < 5; i++) {
+    let computerSelection = getComputerSelection();
+
+    // Play a round and get the result
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
+
+    // Update scores based on result
+    if (result.includes("Win")) {
+      playerScore++;
+    } else if (result.includes("Lose")) {
+      computerScore++;
+    }
+  }
+
+  // Announce the winner
+  if (playerScore > computerScore) {
+    console.log(`You won! ${playerScore} to ${computerScore}`);
+  } else if (computerScore > playerScore) {
+    console.log(`You lost. ${computerScore} to ${playerScore}`);
+  } else {
+    console.log("It was a tie!");
+  }
 }
-}
+
 
 console.log(playRound("rock", "scissors")); // You Win! Rock beats Scissors
 console.log(playRound("lizard", "rock")); // You Lose! Rock beats Lizard
